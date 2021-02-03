dda9bbbcd2dd18b2876cb69e94cd3f506652052f
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-03 16:49:39 +0100

    added babel

---
+++ babel.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "presets": ["@babel/preset-env"],
+  "plugins": [
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-private-property-in-object",
+    "@babel/plugin-proposal-private-methods",
+    "@babel/plugin-transform-classes",
+    "@babel/plugin-transform-regenerator",
+    "@babel/plugin-transform-runtime"
+  ]
+}
