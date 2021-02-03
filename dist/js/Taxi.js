9e4021b95686303977460502b159bc2fc4538111
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-03 18:56:55 +0100

    temp

--- dist/js/Taxi.js
+++ dist/js/Taxi.js
@@ -2,6 +2,11 @@
 
 var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
 var _typeof2 = _interopRequireDefault(require("@babel/runtime/helpers/typeof"));
 
 var _toConsumableArray2 = _interopRequireDefault(require("@babel/runtime/helpers/toConsumableArray"));
@@ -434,3 +439,7 @@ var _injectOnClicKListeners2 = function _injectOnClicKListeners2() {
     });
   });
 };
+
+// var _default = Taxi;
+// exports.default = _default;
+exports.default = Taxi;
