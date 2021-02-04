3f379f68cdb12b933798cff8d31e69eb22c04ddf
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-04 12:09:20 +0100

    formatted data

---
+++ assets/data/dataset2.js
@@ -0,0 +1,13 @@
+export default [
+  `“Their occupations included a United States senator, a gangster, rapper, bankers, tradesmen, and even a dolphin trainer. The one thing they all had in common was the fact that they'd journeyed in the back of my London taxi.`,
+  `“At the violet hour, when the eyes and back / Turn upward from the desk / when the human engine waits / like a taxi throbbing waiting.`,
+  `“She likes taxis. She travels in buses and subways when she is trying to stop smoking`,
+  `“No matter how many times I visit this great city, I'm always struck by the same thing: a yellow taxi cab.`,
+  `“As the taxi took me away, I sat quietly in the back, wondering what was next.`,
+  `“At least they looked that way to me / from the taxi window / and since I happened to be sitting / that fading Sunday afternoon / in the very center of the universe.`,
+  `“When I spoke to the taxi driver; my tone was rustic and wandering; instructing him to drift me deep into the hills; poignantly embrace the winds of nature.`,
+  `“Life is like a taxi. The meter just keeps a ticking whether you are getting somewhere or just standing still.`,
+  `“There used to be a strong belief that if you wanted to know what was really going on in a country, the best thing to do was to go there and ask a taxi driver.`,
+  `“The one thing you shouldn't do is try to tell a cab driver how to get somewhere.`,
+  `“Driving a cab is not really a nurturing type of relationship. You take people and they tip you, they may not tip you, you don't know their names, they don't care about you, you don't care about them.`,
+];
