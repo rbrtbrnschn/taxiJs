862d70ab1b45e238f3bb465306792de6ddab8fe6
rbrtbrnschn <robert.bornschein@mailbox.tu-dresden.de>
2021-02-04 12:09:08 +0100

    added logic for examples

--- demo/script.js
+++ demo/script.js
@@ -1,5 +1,8 @@
 import data from "./assets/data/data.js";
-// Custom
+import dataset1 from "./assets/data/dataset1.js";
+import dataset2 from "./assets/data/dataset2.js";
+
+// Home
 (async function () {
   // taxi-auto
   const taxis = document.querySelectorAll("input.taxi");
@@ -14,20 +17,72 @@ import data from "./assets/data/data.js";
       </div>`,
     data: data,
     minChar: 1,
-    query: (entry, value) =>
-    {
+    query: (entry, value) => {
       const both = `${entry.make} ${entry.model}`;
-      if(entry.model.toLowerCase().includes(value.toLowerCase())) {
+      if (entry.model.toLowerCase().includes(value.toLowerCase())) {
         return true;
-      }
-      else if(entry.make.toLowerCase().includes(value.toLowerCase())) {
+      } else if (entry.make.toLowerCase().includes(value.toLowerCase())) {
         return true;
-      }
-      else if(both.toLowerCase().includes(value.toLowerCase())){
+      } else if (both.toLowerCase().includes(value.toLowerCase())) {
         return true;
       }
       return false;
-    }  
+    },
   };
   const _taxi = new Taxi(taxis[0], taxi, options);
+})();
+
+// Examples 1
+(function () {
+  // fuzzy search
+  const $input = document.querySelector("input.taxi#taxi1");
+  const $taxi = document.querySelector("taxi#🚖1");
+  const options = {
+    minChar: 1,
+    query: Taxi.Query.fuzzy,
+    data: dataset1,
+  };
+
+  const _taxi = new Taxi($input, $taxi, options);
+})();
+
+// Examples 2
+(function () {
+  // custom html
+  const $input = document.querySelector("input.taxi#taxi2");
+  const $taxi = document.querySelector("taxi#🚖2");
+  const options = {
+    minChar: 3,
+    query: Taxi.Query.fuzzy,
+    data: dataset2,
+    toHtml: (entry) => `
+    <div class="card" value="${entry}" onclick="console.log(\`${entry}\`)">
+  <div class="card-content" value="${entry}">
+    <p class="title">
+      ${entry}
+    </p>
+    <p class="subtitle">
+      Some person
+    </p>
+  </div>
+  
+</div>`,
+  };
+
+  const _taxi = new Taxi($input, $taxi, options);
+})();
+
+// Examples 2
+(function () {
+  // custom html
+  const $input = document.querySelector("input.taxi#taxi3");
+  const $taxi = document.querySelector("taxi#🚖3");
+  const options = {
+    minChar: 1,
+    data: dataset1,
+    toHtml: Taxi.ToHtml.classic,
+    query: (entry, val) => !entry.toLowerCase().includes(val.toLowerCase()),
+  };
+
+  const _taxi = new Taxi($input, $taxi, options);
 })();
